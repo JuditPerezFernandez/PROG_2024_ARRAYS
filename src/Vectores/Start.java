cf3273b16746d2189681520ead631ff53d935788
Admin <Admin@LAPTOP-O93FSHCL>
2024-01-09 09:46:19 +0100

    Segundo cambio: el start ya no tiene datos

--- src/Vectores/Start.java
+++ src/Vectores/Start.java
@@ -18,16 +18,7 @@ public class Start {
 		// System.out.println(temperaturas.length);
 
 		char frase[] = new char[10];
-		frase[0] = 'H';
-		frase[1] = 'O';
-		frase[2] = 'L';
-		frase[3] = 'A';
-		frase[4] = ' ';
-		frase[5] = 'h';
-		frase[6] = 'o';
-		frase[7] = 'l';
-		frase[8] = 'a';
-		frase[9] = '!';
+		
 
 		for (int i = 0; i < frase.length; i++) {
 			// System.out.println("pos "+i+" = "+frase[i]);
