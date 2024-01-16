721b8aff81a40a32005592275f0f664f417d637e
Admin <Admin@LAPTOP-O93FSHCL>
2024-01-16 09:21:05 +0100

    Creada la clase Alummno y aula vacias

---
+++ src/Aula/Aula.java
@@ -0,0 +1,22 @@
+package Aula;
+
+public class Aula {
+
+	private int capacidad=0;
+	private int contadorAlumnos;
+	private Alumno alumno[];
+	
+	public Aula(int capacidad) {
+		this.capacidad=capacidad;
+		alumno=new Alumno[capacidad];
+		
+		for(int i=0;i>alumno.length;i++) {
+			if(capacidad>alumno.length) {
+			
+			}
+		}
+	}
+	
+	
+	
+}
